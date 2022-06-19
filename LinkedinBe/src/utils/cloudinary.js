da587240d929d20d43a516a45b92418bcb5c895a
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-20 01:15:30 +0200

    workin

--- LinkedinBe/src/utils/cloudinary.js
+++ LinkedinBe/src/utils/cloudinary.js
@@ -1,22 +1,22 @@
-import multer from 'multer'
-import { v2 as cloudinary } from 'cloudinary'
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET } = process.env
+const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET } = process.env;
 
 cloudinary.config({
-    cloud_name: CLOUDINARY_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_SECRET
-})
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_SECRET,
+});
 
 const cloudinaryStorage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'bw3-linkedin-be',
-    },
-})
+  cloudinary: cloudinary,
+  params: {
+    folder: "Linkedin",
+  },
+});
 
-const parser = multer({ storage: cloudinaryStorage })
+const parser = multer({ storage: cloudinaryStorage });
 
-export { parser, cloudinary }
+export { parser, cloudinary };
