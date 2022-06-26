0e32818e7b2f96863a0daa4b9bdbd57b4299dfb3
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-26 03:05:36 +0200

    implementing auth

---
+++ LinkedinBe/src/auth/jwt.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import UserRouter from "../user/user";
+
+// Create user Interface
+const JwtAuthenticate = async (user) => {
+  const accessToken = await generateToken(user._id);
+  return accessToken;
+};
+//   generate token
+
+const generateToken = (payload, _id) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1 week" },
+      (err, token) => {
+        if (err) rej(err);
+        else res(token);
+      }
+    )
+  );
+export const verifyToken = (token) =>
+  new Promise((res, rej) =>
+    jwt.verify(token, process.env.JWT_SECRET, (err, token) => {
+      if (err) rej(err);
+      else res(token);
+    })
+  );
