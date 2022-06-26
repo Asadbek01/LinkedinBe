37676c048488be82c821323e2061cb96fd4d8999
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-26 02:32:20 +0200

    Crud is ready for user

--- LinkedinBe/src/user/user.js
+++ LinkedinBe/src/user/user.js
@@ -1,4 +1,5 @@
 import express from "express";
+import createHttpError from "http-errors";
 import UserModel from "../user/schema.js";
 const UserRouter = express.Router();
 // 1
@@ -14,6 +15,8 @@ UserRouter.post("/new", async (req, res, next) => {
 // 2
 UserRouter.get("/", async (req, res, next) => {
   try {
+    const user = await UserModel.find();
+    res.send(user);
   } catch (error) {
     next(error);
   }
@@ -21,6 +24,12 @@ UserRouter.get("/", async (req, res, next) => {
 // 3
 UserRouter.get("/:id", async (req, res, next) => {
   try {
+    const user = await UserModel.findById(req.params.id);
+    if (!user)
+      return next(
+        createHttpError(404, `The user with ${req.params.id} is not found.`)
+      );
+    res.send(user);
   } catch (error) {
     next(error);
   }
@@ -28,13 +37,27 @@ UserRouter.get("/:id", async (req, res, next) => {
 // 4
 UserRouter.put("/:id", async (req, res, next) => {
   try {
+    const user = await UserModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user)
+      return next(
+        createHttpError(404, `The user with ${req.params.id} is not found.`)
+      );
+    res.send(user);
   } catch (error) {
     next(error);
   }
 });
 // 5
-UserRouter.get("/:id", async (req, res, next) => {
+UserRouter.delete("/:id", async (req, res, next) => {
   try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    user
+      ? res.send(user)
+      : next(
+          createHttpError(404, `The user with ${req.params.id} is not found.`)
+        );
   } catch (error) {
     next(error);
   }
