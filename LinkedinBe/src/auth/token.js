2d1297ab8f41826fbcd7122a608eeda6862cb6c8
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-26 12:21:44 +0200

    done

--- LinkedinBe/src/auth/token.js
+++ LinkedinBe/src/auth/token.js
@@ -1,6 +1,6 @@
 import createHttpError from "http-errors";
-import { UserModel } from "../Schema/models";
-import { verifyJWT } from "./tools";
+import UserModel from "../user/schema.js";
+import { verifyToken } from "./jwt.js";
 export const JWTAuthMiddleware = async (req, res, next) => {
   // 1. Check if Authorization header is received, if it is not --> 401
   if (!req.headers.authorization) {
@@ -12,7 +12,7 @@ export const JWTAuthMiddleware = async (req, res, next) => {
       // 2. If we receive Authorization header we extract the token from the header
       const token = req.headers.authorization.replace("Bearer ", "");
       // 3. Verify the token, if everything goes fine we are getting back the _id of the logged in user, otherwise an error will be thrown by jwt library
-      const decodedToken = await verifyJWT(token);
+      const decodedToken = await verifyToken(token);
       console.log("DECODED TOKEN ", decodedToken);
       // 4. Find the user in db and attach him/her to the request object
       const user = await UserModel.findById(decodedToken._id);
