7beb19c5c1f634d61649b0f258e61aa6b1287e84
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-27 11:57:50 +0200

    done

--- src/auth/oauth.js
+++ src/auth/oauth.js
@@ -1,21 +1,22 @@
 import passport from "passport";
 import GoogleStrategy from "passport-google-oauth20";
-import UserModel from "../user/schema.js"
+import UserModel from "../user/schema.js";
 import { JwtAuthenticate } from "./jwt.js";
-const googleStrategy = new GoogleStrategy({
+const googleStrategy = new GoogleStrategy(
+  {
     clientID: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: `${process.env.API_URL}/author/googleRedirect`
-},
-async (accessToken, profile, passportNext) => {
+    callbackURL: `${process.env.API_URL}/user/googleRedirect`,
+  },
+  async (accessToken, profile, passportNext) => {
+    console.log(profile);
     try {
-        
     } catch (error) {
-        
+      console.log(error);
     }
-}
-)
+  }
+);
 passport.serializeUser((data, passportNext) => {
-    passportNext(null, data)
-  })
-export default googleStrategy
+  passportNext(null, data);
+});
+export default googleStrategy;
